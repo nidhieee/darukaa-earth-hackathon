4e0c46deb051d17603aa29dc49c63770783eabf8
Nidhi Tupe <nidhitupe28@gmail.com>
2026-09-18 19:02:27 +0530

    fix: import missing useState hook in ProjectMapView

--- web/src/components/map/ProjectMapView.jsx
+++ web/src/components/map/ProjectMapView.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import MapboxDraw from "@mapbox/mapbox-gl-draw";
 import { useNavigate } from "react-router-dom";
